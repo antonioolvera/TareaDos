abaa3a24e32b8b6f98120408ea72d68e60f6c63c
Antonio Olvera <AntonioOlvera@macbook-air.local>
2016-04-01 00:46:10 -0600

    Initial Commit

---
+++ TareaDos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TareaDos
+//
+//  Created by Antonio Olvera on 01/04/16.
+//  Copyright © 2016 Antonio Olvera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
